cd7538b93a880ae9baf96a26cfeeb0f1a0eb0246
Christopher Ressl <christopherressl@MacBookAirRessl.fritz.box>
2021-07-02 17:48:00 +0200

    added navigation buttons to tab slider

--- includes/S3DiviModules.php
+++ includes/S3DiviModules.php
@@ -28,7 +28,7 @@ class S3DM_S3DiviModules extends DiviExtension {
 	 *
 	 * @var string
 	 */
-	public $version = '1.3.2';
+	public $version = '1.3.3';
 
 	/**
 	 * S3DM_S3DiviModules constructor.
--- s3-divi-modules.php
+++ s3-divi-modules.php
@@ -3,7 +3,7 @@
 Plugin Name: S3 Divi Modules
 Plugin URI:  
 Description: Some Custom Modules based on uikit
-Version:     1.3.2
+Version:     1.3.3
 Author:      S3 Advertising
 Author URI:  https://s3-advertising.com
 License:     GPL2
